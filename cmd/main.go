b3f801191776326f6d394caae255227b3063a721
nuka137 <n.a.sch137@gmail.com>
2021-09-15 02:18:43 +0000

    WIP

--- cmd/main.go
+++ cmd/main.go
@@ -51,11 +51,6 @@ func (options *CommandOptions) Validate() error {
         want bool
         msg string
     }{
-/*        {
-            want: len(options.Args) > 0,
-            msg: "Number of arguments must be > 0",
-        },
-        */
     }
 
     for _, c := range cases {
@@ -89,9 +84,10 @@ func (options *CommandOptions) Run() error {
         }
 
         type NodeInfo struct {
-            NodeName string
-            GpuAllocatable int64
-            GpuRequested int64
+            nodeName string
+            gpuAllocatable int64
+            gpuRequests int64
+            gpuLimits int64
         }
 
         var nodeInfo []NodeInfo
@@ -105,15 +101,18 @@ func (options *CommandOptions) Run() error {
             }
 
             info := &NodeInfo{}
-            info.NodeName = node.Name
+            info.nodeName = node.Name
             gpuAlloc := node.Status.Allocatable["nvidia.com/gpu"]
-            info.GpuAllocatable += gpuAlloc.Value()
+            info.gpuAllocatable += gpuAlloc.Value()
 
-            info.GpuRequested = 0
+            info.gpuRequests = 0
+            info.gpuLimits = 0
             for _, pod := range pods.Items {
                 for _, container := range pod.Spec.Containers {
                     gpuReq := container.Resources.Requests["nvidia.com/gpu"]
-                    info.GpuRequested += gpuReq.Value()
+                    gpuLim := container.Resources.Limits["nvidia.com/gpu"]
+                    info.gpuRequests += gpuReq.Value()
+                    info.gpuLimits += gpuLim.Value()
                 }
             }
 
@@ -122,16 +121,62 @@ func (options *CommandOptions) Run() error {
 
         writer := new(tabwriter.Writer)
         writer.Init(os.Stdout, 0, 8, 0, '\t', 0)
-        fmt.Fprintf(writer, "NODE NAME\tGPU (Request/Total)\n")
+        fmt.Fprintf(writer, "NODE NAME\tGPU (Requests/Total)\tGPU (Limits/Total)\n")
+        var gpuAllocatableTotal int64 = 0
+        var gpuRequestsTotal int64 = 0
+        var gpuLimitsTotal int64 = 0
         for _, info := range nodeInfo {
-            fmt.Fprintf(writer, "%s\t%d/%d\n",
-                        info.NodeName,
-                        info.GpuRequested,
-                        info.GpuAllocatable)
+            fmt.Fprintf(writer, "%s\t%d/%d\t%d/%d\n",
+                        info.nodeName,
+                        info.gpuRequests,
+                        info.gpuAllocatable,
+                        info.gpuLimits,
+                        info.gpuAllocatable)
+            gpuAllocatableTotal += info.gpuAllocatable
+            gpuRequestsTotal += info.gpuRequests
+            gpuLimitsTotal += info.gpuLimits
         }
+        fmt.Fprintf(writer, "\t\t\n")
+        fmt.Fprintf(writer, "TOTAL\t%d/%d\t%d/%d\n", gpuRequestsTotal, gpuAllocatableTotal, gpuLimitsTotal, gpuAllocatableTotal)
         writer.Flush()
     } else {
-        fmt.Printf("TBD\n")
+        pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+        if err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+
+        type PodInfo struct {
+            podName string
+            gpuRequest int64
+            gpuLimit int64
+        }
+
+        var podInfo []PodInfo
+        for _, pod := range pods.Items {
+            info := &PodInfo{}
+            info.podName = pod.Name
+            for _, container := range pod.Spec.Containers {
+                gpuReq := container.Resources.Requests["nvidia.com/gpu"]
+                gpuLim := container.Resources.Limits["nvidia.com/gpu"]
+                info.gpuRequest += gpuReq.Value()
+                info.gpuLimit += gpuLim.Value()
+            }
+            podInfo = append(podInfo, *info)
+        }
+
+        writer := new(tabwriter.Writer)
+        writer.Init(os.Stdout, 0, 8, 0, '\t', 0)
+        fmt.Fprintf(writer, "POD NAME\tGPU (Request)\tGPU (Limit)\n")
+        for _, info := range podInfo {
+            if info.gpuRequest > 0 && info.gpuLimit > 0 {
+                fmt.Fprintf(writer, "%s\t%d\t%d\n",
+                            info.podName,
+                            info.gpuRequest,
+                            info.gpuLimit)
+            }
+        }
+        writer.Flush()
     }
 
 
@@ -145,7 +190,7 @@ func main() {
     }
 
     rootCmd := &cobra.Command{
-        Use: "kubectl-sample-plugin",
+        Use: "kubectl-gpu-capacity",
         SilenceUsage: true,
         RunE: func(cmd *cobra.Command, args []string) error {
 
